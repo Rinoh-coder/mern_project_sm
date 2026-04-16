be9f2772d315c600b13630d09b5350f0cae40ab2
rinoh <9965rinoh@gmail.com>
2026-04-16 13:43:33 +0300

    je viens d'ajouter le cryptage bcrypt mais un bugg incomprehensible et non trouvé jusqu'ici fait que le test sur postman echoue, du coup j'ai mis en commentaire le bout du code bcrypt pour l'encryptage du mot de passe

--- models/user.model.js
+++ models/user.model.js
@@ -1,50 +1,61 @@
-const mongoose = require('mongoose');
-const { isEmail } = require('validator');
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
-    {
-        pseudo: {
-            type: String,
-            required: true,
-            minLength: 3,
-            maxLength: 60,
-            unique: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            validate: [isEmail],
-            lowercase: true,
-            unique: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: true,
-            max: 1024,
-            minLength: 6,
-        },
-        bio: {
-            type: String,
-            max: 1024,
-        },
-        followers: {
-            type: [String]
-        },
-        following: {
-            type: [String]
-        },
-        likes: {
-            type: [String]
-        }
+  {
+    pseudo: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 60,
+      unique: true,
+      trim: true,
     },
-    {
-        timestamps: true  // Ajoute createdAt et updatedAt automatiquement
-    }
+    email: {
+      type: String,
+      required: true,
+      validate: [isEmail],
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      minLength: 6,
+    },
+    picture: {
+      type: String,
+      default: "./uploads/profil/random-user.png",
+    },
+    bio: {
+      type: String,
+      max: 1024,
+    },
+    followers: {
+      type: [String],
+    },
+    following: {
+      type: [String],
+    },
+    likes: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true, // Ajoute createdAt et updatedAt automatiquement
+  },
 );
 
+/** 
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+}); 
+*/
 
-
-const UserModel = mongoose.model('user', userSchema);
+const UserModel = mongoose.model("user", userSchema);
 module.exports = UserModel;
