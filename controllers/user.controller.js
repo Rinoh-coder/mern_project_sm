49446710361f66bdad57a06915380f47b54ca80c
rinoh <9965rinoh@gmail.com>
2026-04-17 18:06:24 +0300

    ajout du route qui va ous permettre de prendre un utilisateur donné par son id et retourner ses onformations non sensibles avec userInfo, utilisation de objectID pour valider l'id dans req.params.id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,23 @@
 const UserModel = require('../models/user.model');
-const ObjectId = require('mongoose').Types.ObjectId;
+const ObjectID = require('mongoose').Types.ObjectId;
 
 module.exports.getAllUsers = async(req, res) => {
     const users = await UserModel.find().select('-password');
     res.status(200).json(users);
 };
+
+module.exports.userInfo = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        return res.status(400).send('ID unknown : ' + req.params.id);
+    }
+    try {
+        const docs = await UserModel.findById(req.params.id).select('-password');
+        if (!docs) {
+            return res.status(404).send('User not found');
+        }
+        res.send(docs);
+    } catch (err) {
+        console.log('Erreur : ' + err);
+        res.status(500).send('Internal server error');
+    }
+}
