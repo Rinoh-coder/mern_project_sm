54dab66fe2dbafdd698557f9faff9209cea3498f
rinoh <9965rinoh@gmail.com>
2026-04-17 17:21:52 +0300

    ajout du fichier user.controller pour géer les CRUD pour la table users, on vient d'ajout le get pour prendre tous les utilisateurs dans la table, avec un select('-password') pour ne pas renvoyer le password des utilisateurs

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,14 @@
 
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
+const userController = require('../controllers/user.controller');
 
+
+//auth
 router.post("/register", authController.signUp);
+
+
+//user database
+router.get("/", userController.getAllUsers);
 
 module.exports = router ;
