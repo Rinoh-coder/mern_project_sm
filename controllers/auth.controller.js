e997e1f33668c4bdeacab536fa2c4407f57afed5
rinoh <9965rinoh@gmail.com>
2026-04-21 00:24:18 +0300

    ajout de la gestion d'erreur au niveau de signIn et correction d'un petit bugg au niveau de signUpErrors

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,16 @@
 // auth.controller.js
 
-const jwt = require('jsonwebtoken');
-require('dotenv').config({path: './config/.env'});
-const UserModel = require('../models/user.model');
-//const signUpErrors = require('../utils/errors.utils');
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "./config/.env" });
+const UserModel = require("../models/user.model");
+const { signUpErrors, signInErrors } = require("../utils/errors.utils");
 
-const maxAge = 3*24*60*60
+const maxAge = 3 * 24 * 60 * 60;
 
 // create token
- const createToken = (id) => {
-  return jwt.sign({id}, process.env.TOKEN_SECRET, { expiresIn : maxAge})
-  };
-
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, { expiresIn: maxAge });
+};
 
 // signUp
 module.exports.signUp = async (req, res) => {
@@ -22,27 +21,26 @@ module.exports.signUp = async (req, res) => {
     res.status(201).json({ user: user._id });
   } catch (err) {
     const errors = signUpErrors(err);
-    res.status(400).send({errors});
+    res.status(400).send({ errors });
   }
 };
 
-
 // signIn
 module.exports.signIn = async (req, res) => {
-  const { email, password } = req.body
-  try{
+  const { email, password } = req.body;
+  try {
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly : true, maxAge : maxAge});
-    res.status(200).json({user : user._id});
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge });
+    res.status(200).json({ user: user._id });
   } catch (err) {
-    return res.status(400).json({ error : err.message})
+    const errors = signInErrors(err);
+    return res.status(400).json({ errors });
   }
-  };
-
+};
 
 // logout
 module.exports.logout = (req, res) => {
-  res.cookie('jwt', '', { maxAge: 1 }); // Supprime le cookie
-  res.status(200).json({ message: 'Logged out' });
+  res.cookie("jwt", "", { maxAge: 1 }); // Supprime le cookie
+  res.status(200).json({ message: "Logged out" });
 };
--- utils/errors.utils.js
+++ utils/errors.utils.js
@@ -1,15 +1,9 @@
 // errors.utils.js
 
+
+//signUpErrors
 module.exports.signUpErrors = (err) => {
 	let errors = { pseudo : '', email : '', password : ''}
-
-
-    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('pseudo')) {
-        errors.pseudo = 'Pseudo déjà utilisé ';
-    }
-    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('email')) {
-        errors.email = 'Email déjà utilisé ';
-    }
 
     if (err.message.includes('pseudo')) {
         errors.pseudo = 'Pseudo trop court';
@@ -20,7 +14,26 @@ module.exports.signUpErrors = (err) => {
     if (err.message.includes('password')) {
         errors.password = 'Le mot de passe doit faire minimum 6 caractères';
     }
+    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('pseudo')) {
+        errors.pseudo = 'Pseudo déjà utilisé ';
+    }
+    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes('email')) {
+        errors.email = 'Email déjà utilisé ';
+    }
+    return errors;
 
+}
+
+//signInErrors
+module.exports.signInErrors = (err) => {
+	let errors = { email : '', password : ''}
+
+    if (err.message.includes('email')) {
+        errors.email = 'Email inconnu';
+    }
+    if (err.message.includes('password')) {
+        errors.password = 'Le mot de passe est incorrect';
+    }
 
     return errors;
 
