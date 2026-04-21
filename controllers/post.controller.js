40a4df9e8645d2e0b42e2b0bed3067be7eaafad6
rinoh <9965rinoh@gmail.com>
2026-04-21 23:36:42 +0300

    ajout de createPost avec validation et gestion d'erreur, createPost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,30 +1,65 @@
 // post.controller.js
 
-const PostModel = require('../models/post.model');
-const UserModel = require('../models/user.model');
-const ObjectID = require('mongoose').Types.ObjectId;
+const PostModel = require("../models/post.model");
+const UserModel = require("../models/user.model");
+const ObjectID = require("mongoose").Types.ObjectId;
 
-
+// readPost
 module.exports.readPost = async (req, res) => {
-    try {
-        const posts = await PostModel.find()
-        if (posts.length === 0) {
-            return res.status(404).send("No post to read !");
-        } return res.status(200).send(posts);
-    } catch (err) {
-        console.log("Erreur : " + err);
-        return res.status(500).send('Internal Server error');
+  try {
+    const posts = await PostModel.find();
+    if (posts.length === 0) {
+      return res.status(404).send("No post to read !");
     }
+    return res.status(200).send(posts);
+  } catch (err) {
+    console.log("Erreur : " + err);
+    return res.status(500).send("Internal Server error");
+  }
 };
 
-module.exports.createPost = (req, res) => {
-    
-}
+// createPost
+module.exports.createPost = async (req, res) => {
 
-module.exports.updatePost = (req, res) => {
-    
-}
+  if (!req.body.posterId) {
+    return res.status(400).send("User Id required");
+  }
 
-module.exports.deletePost = (req, res) => {
-    
-}
+  if (!ObjectID.isValid(req.body.posterId)) {
+    return res.status(400).send("ID unknown : " + req.body.posterId);
+  }
+
+  const userExist = await UserModel.findById(req.body.posterId)
+  if (!userExist) {
+    return res.status(404).send(" User not found");
+  }
+
+  if (!req.body.message) {
+    return res.status(400).send("Message est requis");
+  }
+
+  const newPost = new PostModel({
+    posterId: req.body.posterId,
+    message: req.body.message,
+    video: req.body.video || "",
+    likers: [],
+    comments: [],
+  });
+  try {
+    const post = await newPost.save();
+    return res.status(201).send(post);
+  } catch (err) {
+    console.log("Erreur : " + err);
+    return res.status(400).send(err);
+  }
+};
+
+
+
+//updatePost
+module.exports.updatePost = (req, res) => {};
+
+
+
+// deletePost
+module.exports.deletePost = (req, res) => {};
